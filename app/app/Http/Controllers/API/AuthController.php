1f6991adeeade2419d18b9ebc35af50a5f7d6e25
Felippe Miguel <felippe.miguel.silva@gmail.com>
2022-05-27 09:31:39 -0300

    fix formatting

--- app/app/Http/Controllers/API/AuthController.php
+++ app/app/Http/Controllers/API/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password)
-         ]);
+        ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
