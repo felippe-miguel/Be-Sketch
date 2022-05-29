627ea00b3281ca6f87bceebbfb16f2b2ca016a23
Felippe Miguel <felippe.miguel.silva@gmail.com>
2022-05-29 13:59:47 -0300

    creating base node models

---
+++ app/app/Models/Card.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Node;
+
+class Card extends Node
+{
+}
---
+++ app/app/Models/Column.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Node;
+
+class Column extends Node
+{
+}
