263af76b718d409b3575813f813fdf05be743d68
felippe.miguel.silva@gmail.com <felippe.miguel.silva@gmail.com>
2022-05-29 14:10:53 -0300

    initial relationships

--- app/app/Models/Node.php
+++ app/app/Models/Node.php
@@ -6,4 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Node extends Model
 {
+    public function parent()
+    {
+        return $this->belongsTo(self::class);
+    }
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
 }
