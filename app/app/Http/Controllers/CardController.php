c5e627e7aff53a8fb76837d8cb3552d79c45234b
Felippe Miguel <felippe.miguel.silva@gmail.com>
2022-05-31 17:19:35 -0300

    starting modifications on CardController

--- app/app/Http/Controllers/CardController.php
+++ app/app/Http/Controllers/CardController.php
@@ -3,32 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Card;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class CardController extends Controller
 {
-    public function index(): Response
+    public function index(): JsonResponse
     {
         //
     }
 
-    public function store(Request $request): Response
+    public function store(Request $request): JsonResponse
     {
         //
     }
 
-    public function show(Card $card): Response
+    public function show(Card $card): JsonResponse
     {
         //
     }
 
-    public function update(Request $request, Card $card): Response
+    public function update(Request $request, Card $card): JsonResponse
     {
         //
     }
 
-    public function destroy(Card $card): Response
+    public function destroy(Card $card): JsonResponse
     {
         //
     }
