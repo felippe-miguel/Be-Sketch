ac15ace53fba963538c65cdd772faf27a3b148c6
Felippe Miguel <felippe.miguel.silva@gmail.com>
2022-05-31 16:55:03 -0300

    new logic in base methods for ColumnController

--- app/app/Http/Controllers/ColumnController.php
+++ app/app/Http/Controllers/ColumnController.php
@@ -2,34 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Column\IndexColumnRequest;
+use App\Http\Requests\Column\StoreColumnRequest;
+use App\Http\Requests\Column\UpdateColumnRequest;
 use App\Models\Column;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Gate;
 
 class ColumnController extends Controller
 {
-    public function index(): Response
+    public function index(IndexColumnRequest $request): JsonResponse
     {
-        //
+        $columns = Column::where('board_id', $request->board_id)->get();
+
+        return response()->json([
+            'columns' => $columns->values()->toArray()
+        ]);
     }
 
-    public function store(Request $request): Response
+    public function store(StoreColumnRequest $request): JsonResponse
     {
-        //
+        $column = Column::create($request->validated());
+
+        return response()->json([
+            'message' => 'Column successfully created',
+            'id'      => $column->id
+        ], 201);
     }
 
-    public function show(Column $column): Response
+    public function show(Column $column): JsonResponse
     {
-        //
+        if (!Gate::allows('column-show', $column)) {
+            abort(403);
+        }
+
+        return response()->json($column->toArray());
     }
 
-    public function update(Request $request, Column $column): Response
+    public function update(UpdateColumnRequest $request, Column $column): JsonResponse
     {
-        //
+        if (!Gate::allows('column-update', $column)) {
+            abort(403);
+        }
+
+        if (empty($request->validated())) {
+            return response()->json(['message' => 'Nothing to update.']);
+        }
+
+        $column->fill($request->validated())->save();
+
+        return response()->json(['message' => 'Column successfully updated']);
     }
 
-    public function destroy(Column $column): Response
+    public function destroy(Column $column): JsonResponse
     {
-        //
+        if (!Gate::allows('column-destroy', $column)) {
+            abort(403);
+        }
+
+        $column->delete();
+
+        return response()->json(['message' => 'Column successfully deleted']);
     }
 }
