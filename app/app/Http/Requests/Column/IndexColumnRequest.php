ca3bd8ebedf73da6dab86231b2b880566cfaa141
Felippe Miguel <felippe.miguel.silva@gmail.com>
2022-05-31 16:53:07 -0300

    new indexColumnRequest

---
+++ app/app/Http/Requests/Column/IndexColumnRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests\Column;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class IndexColumnRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'board_id' => [
+                'required',
+                Rule::exists('boards', 'id')->where('user_id', Auth::id())
+            ]
+        ];
+    }
+}
